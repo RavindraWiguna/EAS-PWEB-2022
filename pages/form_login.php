d5aa3d2156193df160a22fcd5bf0f90e7260568c
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-12-18 07:06:34 +0700

    show close password

--- pages/form_login.php
+++ pages/form_login.php
@@ -17,7 +17,8 @@
                 <h5>Isi form dengan akun yang telah terdaftar</h5>
             </div>
             <div class="bg-white rounded px-3 auth-box mx-auto">
-                <form method="POST" action="../assets/php/proses_login.php" onSubmit="return validateLogin()"> 
+                <form method="POST" action="../assets/php/proses_login.php" onSubmit="return validateLogin()">
+                    <p id="msglogin" class="text-danger"></p>
                     <!-- Email/Username input -->
                     <div class="form-outline pt-3">
                         <label class="form-label text-black" for="user_email">Email atau Username</label>
@@ -26,8 +27,13 @@
 
                     <!-- Password input -->
                     <div class="form-outline mt-3">
-                        <label class="form-label text-black" for="password">Password</label>    
-                        <input type="password" name="password" class="form-control" placeholder="Password akun" id="idpassword"/>     
+                        <label class="form-label text-black" for="password">Password</label>
+                        <div class="input-group">
+                            <input type="password" name="password" class="form-control" placeholder="Password akun" id="idpassword"/>
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2" onclick="showPassword()">
+                                <img src="../assets/media/eye-closed.svg" alt="" style="width:16px" id="show-pass-icon">
+                            </button>
+                        </div>
                     </div>
                     
                     <!-- Login and Signin -->
@@ -42,6 +48,21 @@
         </div>
     </div>
     <?php include('components/footer.php');?>
+
+    <script>
+        function showPassword(event) {
+            var x = document.getElementById("idpassword");
+            let icon = document.getElementById("show-pass-icon");
+            if (x.type === "password") {
+                x.type = "text";
+                icon.src ='../assets/media/eye-open.svg';
+            } else {
+                x.type = "password";
+                icon.src ='../assets/media/eye-closed.svg';
+            }
+        }
+    </script>
+
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
