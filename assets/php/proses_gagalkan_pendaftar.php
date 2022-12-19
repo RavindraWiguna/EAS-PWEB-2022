cf896748f762ac2e130a904905b40d81370f5da7
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-12-19 12:51:25 +0700

    save  progress

--- assets/php/proses_gagalkan_pendaftar.php
+++ assets/php/proses_gagalkan_pendaftar.php
@@ -4,6 +4,29 @@ if(!session_id()) session_start();
 //ambil id pendaftar tersimpan di session
 $pendaftar = $_SESSION['pendaftar'];
 
+// cek apakah ada data pendaftar di tabel pendaftar_lolos
+$sql = "SELECT * FROM pendaftar_lolos WHERE id_pendaftar=".$pendaftar['id'];
+
+// jalankan query
+$query = mysqli_query($db, $sql);
+
+// cek jumlah data yang ditemukan
+if( mysqli_num_rows($query) > 0 ){
+    // jika ada, hapus data pendaftar dari tabel pendaftar_lolos
+    $sql = "DELETE FROM pendaftar_lolos WHERE id_pendaftar=".$pendaftar['id'];
+    $query = mysqli_query($db, $sql);
+
+    // cek apakah query gagal
+    if( !$query ){
+        // jika gagal, tampilkan pesan gagal menghapus data pendaftar dari tabel pendaftar_lolos
+        header('Location: ../../pages/lihat_pendaftar.php?id='.$id.'&status=gagal&pesan=gagal');
+        exit();
+    }
+
+}
+
+
+
 // buat query untuk mengupdate data pendaftar pada tabel pendaftar untuk kolom status pendaftaran menjadi -1
 $id = $pendaftar['id'];
 $sql = "UPDATE pendaftar SET status_pendaftaran=-1 WHERE id=$id";
--- assets/php/proses_loloskan_pendaftar.php
+++ assets/php/proses_loloskan_pendaftar.php
@@ -4,19 +4,34 @@ if(!session_id()) session_start();
 //ambil id pendaftar tersimpan di session
 $pendaftar = $_SESSION['pendaftar'];
 
-// buat query untuk mengupdate data pendaftar pada tabel pendaftar untuk kolom status pendaftaran menjadi 1
-$id = $pendaftar['id'];
-$sql = "UPDATE pendaftar SET status_pendaftaran=1 WHERE id=$id";
+// tambahkan pendaftar ke tabel pendaftar lolos dengan kolom id_pendaftar, id_verifier, id_sesi, tanggal_ujian dan id_lokasi
+$sql = "INSERT INTO pendaftar_lolos 
+(id_pendaftar, id_verifier, id_sesi, tanggal_ujian, id_lokasi) 
+VALUES ('".$pendaftar['id']."', '".$_SESSION['user']['id']."', '1', '2023-01-16', '1')";
+
+// jalankan query
 $query = mysqli_query($db, $sql);
 
-// cek apakah query update berhasil
+// cek apakah query berhasil
 if( $query ){
-    // jika berhasil, redirect ke halaman lihat pendafatar
-    header('Location: ../../pages/lihat_pendaftar.php?id='.$id.'&status=berhasil&pesan=lolos');
-    exit();
+    // buat query untuk mengupdate data pendaftar pada tabel pendaftar untuk kolom status pendaftaran menjadi 1
+    $id = $pendaftar['id'];
+    $sql = "UPDATE pendaftar SET status_pendaftaran=1 WHERE id=$id";
+    $query = mysqli_query($db, $sql);
+
+    // cek apakah query update berhasil
+    if( $query ){
+        // jika berhasil, redirect ke halaman lihat pendafatar
+        header('Location: ../../pages/lihat_pendaftar.php?id='.$id.'&status=berhasil&pesan=lolos');
+        exit();
+    }else{
+        // jika gagal, tampilkan pesan gagal menggagalkan pendafatar
+        header('Location: ../../pages/lihat_pendaftar.php?id='.$id.'&status=gagal&pesan=lolos');
+        exit();
+    }
 }else{
     // jika gagal, tampilkan pesan gagal menggagalkan pendafatar
-    header('Location: ../../pages/lihat_pendaftar.php?id='.$id.'&status=gagal&pesan=lolos');
+    header('Location: ../../pages/lihat_pendaftar.php?id='.$pendaftar['id'].'&status=gagal&pesan=lolos');
     exit();
 }
 ?>
