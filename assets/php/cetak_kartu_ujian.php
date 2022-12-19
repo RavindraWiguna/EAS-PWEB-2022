41a6666c125955ebe57ec322adca2ffa40bd2db7
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-12-19 15:36:34 +0700

    finish cetak kartu

--- assets/php/cetak_kartu_ujian.php
+++ assets/php/cetak_kartu_ujian.php
@@ -12,7 +12,14 @@ include('../../config.php');
 include('barcode.php');
 
 // ambil data pendaftar dari tabel pendaftar
-$sql = 'SELECT * FROM pendaftar WHERE id_akun = '.$_SESSION['user']['id'];
+$sql = 'SELECT p.path_foto, p.nik, p.id, p.nama, p.jenis_kelamin, p.tempat_lahir, p.tanggal_lahir, p.kualifikasi_pendidikan, tl.nama_dinas, tl.alamat_dinas
+        FROM pendaftar as p
+        JOIN pendaftar_lolos as pl
+        ON pl.id_pendaftar=p.id
+        JOIN tabel_lokasi as tl
+        ON tl.id=pl.id_lokasi
+        WHERE p.id_akun='.$_SESSION['user']['id'].'
+        ';
 $query = mysqli_query($db, $sql);
 $pendaftar = mysqli_fetch_assoc($query);
 
@@ -46,76 +53,60 @@ $code_type = 'Code128';
 $print  = true;
 $sizefactor = 1;
 if(is_file($filepath)) unlink($filepath);
-barcode( $filepath, $text, $size, $orientation, $code_type, $print, $sizefactor );
+barcode( $filepath, generate_no_peserta($pendaftar['id']), $size, $orientation, $code_type, $print, $sizefactor );
 
 $pdf->Image($filepath, 230, 40, 60, 18, 'png');
+$pdf->Image('../..'.$pendaftar['path_foto'], 230, 60, 40, 60, 'jpg');
 
 // membuat header tabel
 $width_col1 = 50;
-$gap = 4;
+$gap = 3.5;
 $pdf->SetFont('Arial','',12);
 $pdf->Cell($width_col1,6,'Instansi:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(72,6,'Kementrian Kelautan dan Perikanan',0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Lokasi:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Multicell(150,6,'Pusat Pengendalian Mutu, Badan Karantina Ikan, Pengendalian Mutu dan Keamanan Hasil Perikanan',0,'');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'NIK:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(64,6,$pendaftar['nik'],0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Nomor Peserta:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(64,6,generate_no_peserta($pendaftar['id']),0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Nama:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Multicell(150,6,$pendaftar['nama'],0,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Jenis Kelamin:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(64,6,$pendaftar['jenis_kelamin'],0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Tempat/Tanggal Lahir:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(64,6,$pendaftar['tempat_lahir'].'/'.$pendaftar['tanggal_lahir'],0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Kualifikasi Pendidikan:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(64,6,$pendaftar['kualifikasi_pendidikan'],0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Formasi Jabatan:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
+$pdf->Cell(64,6,'PENGAWAS PERIKANAN PERTAMA',0,1,'L');
 $pdf->Cell(10,$gap,'',0,1);
 $pdf->Cell($width_col1,6,'Lokasi Tes:',0,0,'');
-$pdf->Cell(64,6,'Kementrian Kelautan dan Perikanan',0,1,'C');
-$pdf->Cell(10,$gap*2,'',0,1);
+$pdf->Multicell(150,6,$pendaftar['nama_dinas'].', '.$pendaftar['alamat_dinas'],0,'l');
+$pdf->Cell(10,$gap+2,'',0,1);
 $pdf->Cell(250,6,'........................................................, 2023',0,1, 'R');
-$pdf->Cell(145,6,'Peserta',0,0,'C');
-$pdf->Cell(145,6,'Panitia Pengadaan Instansi CPNS',0,1,'C');
-$pdf->Cell(10,$gap*2,'',0,1);
-$sx = 48;
-$len = 70;
-$y = 175;
-$pdf->Line($sx, $y,$sx+$len, $y);
+$pdf->Cell(100,6,'Peserta',0,0,'C');
+$pdf->Cell(150,6,'Panitia Pengadaan Instansi CPNS',0,1,'R');
+$pdf->Cell(10,$gap+12,'',0,1);
+$pdf->Cell(100,6,'........................................................',0,1,'C');
 
-$pdf->Cell(10,$gap+8,'',0,1);
-$pdf->Cell(145,6,'PUTU RAVINDRA WIGUNA',0,0,'C');
+$pdf->Cell(100,6,$pendaftar['nama'],0,0,'C');
 
 
+function generate_no_peserta($id){
+    $no_peserta = '0000-000-000000';
+    $no_peserta = substr($no_peserta, 0, -strlen($id)).$id;
+    return $no_peserta;
+}
 
-// untuk koneksi ke database
-// include ("config.php");
-// // melakukan query untuk mengambil seluruh data calon siswa
-// $query = "SELECT * FROM calon_siswa";
-// $result = mysqli_query($db, $query);
-// if(!$result){
-//     die("Gagal mengambil data");
-// }
-// // query berhasil, tambahkan data tersebut pada pdf
-// $pdf->SetFont('Arial', '', 12);
-// while($siswa = mysqli_fetch_array($result)){
-//     $pdf->Cell(10,6,$siswa['id'],1,0);
-//     $pdf->Cell(64,6,$siswa['nama'],1,0);
-//     $pdf->Cell(75,6,$siswa['alamat'],1,0);
-//     $pdf->Cell(34,6,$siswa['jenis_kelamin'],1,0);
-//     $pdf->Cell(30,6,$siswa['agama'],1,0);
-//     $pdf->Cell(64,6,$siswa['sekolah_asal'],1,1);
-// }
-// selesai
+
 $pdf->Output();
 ?>
